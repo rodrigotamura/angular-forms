a1b103b415080dd06c7325b41a370c694c53277b
tamura <ti.tamura@restauramundo.com>
2017-07-18 23:42:28 -0300

    Aula 90 e poucos

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,6 +1,6 @@
 import { Http } from '@angular/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -18,8 +18,8 @@ export class DataFormComponent implements OnInit {
 
   ngOnInit() {
     this.formulario = this.formBuilder.group({
-      nome: [null],
-      email: [null]
+      nome: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]]
     });
 
     /*
@@ -30,12 +30,39 @@ export class DataFormComponent implements OnInit {
     */
   }
 
+  verificaValidTouched(campo){
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  }
+
+  verificaEmailInvalido(){
+    let campoEmail = this.formulario.get('email');
+    if(campoEmail.errors){
+      return campoEmail.errors['email'] && campoEmail.touched;
+    }
+
+  }
+  aplicaCssErro(campo){
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    }
+  }
+
   onSubmit() {
     console.log(this.formulario.value);
 
     this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
       .map(res => res)//Mapeando resposta que vem do servidor;
-      .subscribe(dados => console.log(dados));
+      .subscribe(dados => {
+        console.log(dados);
+        //reseta o form
+        this.resetar();
+      },
+
+      (erro: any) => alert('erro'));
   }
 
+  resetar() {
+    this.formulario.reset();
+  }
 }
